98ab2fbbdba3c2d7686268084057e801141bdc83
mrsaqibale <kalikali72547254@gmail.com>
2024-03-10 10:13:19 +0500

    appply limit on input field only a's b's enter and lock field 1

--- automata/views.py
+++ automata/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+from automata.algo_imp import code1
 # Create your views here.
 
 def test(request):
     if request.method == 'GET':
         btn_val = request.GET.get('input-value')
-        print(btn_val)
+        btn_algo = request.GET.get('input-algo')
+        if(btn_algo == "code1"):
+            {
+            print(code1.fun(btn_val))
+        }
     return render(request,'home.html')
+    
+
