2527f1aa1c2f45d899cbe86053437e10fe4563e9
mrsaqibale <kalikali72547254@gmail.com>
2024-03-14 05:16:29 +0500

    set even-even algo for thml and function

--- automata/views.py
+++ automata/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from automata.algo_imp import start_with_a
+from automata.algo_imp import even_even
 # Create your views here.
 data = {}
 def home(request):
@@ -9,6 +10,15 @@ def home(request):
         # START WITH A ALGO PART01
         if(btn_algo == "start_with_a"):
             values, exps = (start_with_a.check_value(btn_val))
+            data={
+                'value':values,
+                'exp':exps
+                }
+            return render(request, 'show.html',data)
+
+            # even even algo runing
+        if(btn_algo == "even_even"):
+            values, exps = (even_even.check_value(btn_val))
             data={
                 'value':values,
                 'exp':exps
